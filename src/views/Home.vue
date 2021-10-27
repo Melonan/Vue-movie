e0b9e7ee2fba097fc449659f4d21d5ce9c91869a
Lonan <1422872473@qq.com>
2021-10-27 20:44:42 +0800

    last version we still need to learn sass layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,43 +3,62 @@
 
       <div class="feature-card">
 
-        <router-link to="/movie/asuka">
+        <router-link to="/movie/tt0169858">
           <img src="https://gss3.bdstatic.com/84oSdTum2Q5BphGlnYG/timg?wapp&quality=80&size=b150_150&subsize=20480&cut_x=0&cut_w=0&cut_y=0&cut_h=0&sec=1369815402&srctrace&di=d7a0b45676a92f73b610fd9c2ef181fa&wh_rate=null&src=http%3A%2F%2Ftiebapic.baidu.com%2Fforum%2Fpic%2Fitem%2F4610b912c8fcc3ce7b4607548545d688d53f20d7.jpg" 
           alt="Asuka" class="featured-img">
           <div class="detail">
             <h3>Asuka</h3>
-            <p>Asuka , wo zhen de hao xi huan ni a ! wei le ni wo yao ...</p>
+            <p>WUHU! TAKE OFF!</p>
           </div>
         </router-link>
 
       </div>
 
-      <form @submit.prevent="" class="search-box">
-        <input type="text" placeholder="What are you looking for" />
+      <form @submit.prevent="SearchMovies()" class="search-box">
+        <input type="text" placeholder="What are you looking for" v-model="search" />
         <input type="submit" value="Search">
       </form>
 
-      <div class="movies-list">MOVIES</div>
+<!-- show the movies we searched -->
+      <div class="movies-list">
+        <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+          <router-link :to="'/movie/'+movie.imdbID" class="movie-link">
+            <div class="product-image">
+              <img :src="movie.Poster" alt="Movie Poster">
+              <div class="type">
+                {{movie.Type}}
+              </div>
+            </div>
+            <div class="detail">
+              <p class="year">{{ movie.Year }}</p>
+              <h3 >{{movie.Title}}</h3>
+            </div>
+          </router-link>
+        </div>
+      </div>
   </div>
 </template>
 
-<script>
 
-import { ref } from 'vue'
-// import env from '@/env.js'
+<script>
+import { ref } from 'vue';
+import env from '@/env.js'
+
 export default {
-  setup(){
+  setup () {
     const search = ref("");
     const movies = ref([]);
 
     const SearchMovies = () => {
-      if(search.value != "")
-      {
-        fetch(`http://www.omdbapi.com/?apikey=17503e95&s=${search.value}`)
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
           .then(response => response.json())
           .then(data => {
-            // console.log(`${env.apikey}`)
             console.log(data);
+            movies.value = data.Search;
+            // 点击search按钮之后就reset掉刚刚输入的内容 , 即将search.value重新置为空 (v-model)绑定过了嘛
+            search.value = "";
+
           });
       }
     }
@@ -50,7 +69,6 @@ export default {
       SearchMovies
     }
   }
-
 }
 </script>
 
@@ -178,5 +196,73 @@ export default {
     }
 
   }
+
+
+  .movies-list{
+    display: flex;
+    flex-wrap: wrap;
+    // 0px : 上下
+    // 8px : 左右
+    margin: 0px 8px;
+
+    .movie{
+      max-width: 50%;
+// flex的三个参数
+// flex-grow	一个数字，规定项目将相对于其他灵活的项目进行扩展的量。
+// flex-shrink	一个数字，规定项目将相对于其他灵活的项目进行收缩的量。
+// flex-basis	项目的长度。合法值："auto"、"inherit" 或一个后跟 "%"、"px"、"em" 或任何其他长度单位的数字。
+      flex:1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link{
+        // movie lik 里面有 {图片+type} {标题+年份}
+        display: flex;
+        flex-direction: column;
+        // height: 100%;
+
+        .product-image{
+          position: relative;
+          display: block;
+
+          img{
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type{
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+          
+        }
+
+        .detail{
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+          
+          .year{
+            color: #AAA;
+            font-size: 14px;
+          }
+
+          h3{
+            color: #fff;
+            font-weight: 600;
+          }
+        }
+      }
+    }
+  }
+
+
 }
 </style>
--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,16 +1,89 @@
 <template>
   <div class="movie-detail">
-      Detail
-      {{ $route.params.id}}
+      <!-- 可以在这里直接用setup里面的变量 -->
+      <h2>{{movie.Title}}</h2>
+      <p>{{movie.Year}}</p>
+
+      <img :src="movie.Poster" alt="Movie Poster" class="featured-img">
+
+      <p>{{movie.Plot}}</p>
+      <p class="movie-release">{{movie.Released}}</p>
   </div>
 </template>
 
 <script>
-export default {
 
+// setup实际上就是create() 生命周期钩子函数 在这个时候还没有data对象 ,
+// 想要使用变量的话就不得不自己定义, 想要变成响应式就不得不使用ref
+// ref 将括号内的东西变成响应式的, 
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router'
+import env from '@/env.js'
+export default {
+    setup()
+    {
+        const movie = ref({});
+        const route = useRoute();
+
+
+        onBeforeMount(()=>{
+            // 这里必须使用useRoute获取route对象
+            fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                movie.value=data;
+            }) 
+        })
+
+        return {
+            movie,
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+    .movie-detail{
+        display: flex;
+        // position: relative;
+        flex-direction: column;
+        // justify-items: center;
+        align-items: center;
+        // align-content: center;
+        padding: 8px 16px;
+        width: 100%;
+        color: rgb(23, 160, 223);
 
+        h2{
+            color: #fff;
+            font-family: '';
+            font-size: 28px;
+            font-weight: 500;
+            margin-bottom: 16px;
+        }
+
+        .featured-img{
+            position: relative;
+            display: block;
+            max-width: 300px;
+            margin-top: 10px;
+            margin-bottom: 20px;
+        }
+
+        p{
+            // color: #fff;
+            font-size: 18px;
+            font-weight: 200;
+            line-height: 1.4;
+
+            
+        }
+        .movie-release{
+            color: #0c040431;
+            font-size: 20px;
+            font-weight: 700;
+        }
+
+    }
 </style>
